6f864bef6cb1cc9ed51342c39eecb4f85311ba43
typhonrt <support@typhonrt.org>
2016-03-13 13:43:14 -0700

    SystemJS has code coverage!

--- src/cjs/cjs.js
+++ src/cjs/cjs.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 var _ = require('underscore');
 
 /**
--- src/esm/esm.js
+++ src/esm/esm.js
@@ -5,7 +5,15 @@ import _ from 'underscore';
  */
 class Test
 {
+   /**
+    * A simple test using Underscore.
+    *
+    * @returns {string}
+    */
    run() { return _.extend({ test: 'testvalue' }, { test2: 'testvalue2' }); }
 }
 
+/**
+ * Exports an instance of Test.
+ */
 export default new Test();
